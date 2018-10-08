42ed40c84ec2a65f50f369774bab026348a6d15a
Stella Neser <stella.neser@googlemail.com>
2018-10-08 11:39:26 +0200

    Ende Kapitel 2

--- Doodle_PP/src/Kapitel_2/DaemonThread.java
+++ Doodle_PP/src/Kapitel_2/DaemonThread.java
@@ -1,6 +1,11 @@
 package Kapitel_2;
+
 /**
  * CB 2.9 Ein Benutzerdefinierter Daemon-Thread
+ * 
+ * DaemonThreads können jederzeit gestopppt werden
+ * Terminieren alle User-Threads, wird der prozess bzw. das Progrmm beendet
+ * egal ob Daemon-Threads noch aktiv laufen 
  * @author Stella
  *
  */
@@ -8,9 +13,13 @@ public class DaemonThread {
 
 	public static void main(String[] args) {
 		Thread clock = new Thread(new ClockDaemon(), "ClockThread");
+		System.out.println("about to start the thread");
 		clock.setDaemon(true);
 		clock.start();
-		//Aktivitäten des main-Threads
+		/** Hier Break-Point für Debuggen, 
+		 * anosnten terminiert das Programm zu schnell und man sieht kein Ergebnis 
+		 */
+		System.out.println("started Thread");
+		// Aktivitäten des main-Threads
 	}
-
 }
